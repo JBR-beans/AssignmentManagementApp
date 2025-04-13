b88539b503c9efb0f131b2fbb0cdc61226b02f9f
RustyBeans <107262647+RustyBeans@users.noreply.github.com>
2025-04-13 13:06:46 -0700

    created tests and fixed project references

--- AssignmentManagementApp/Assignment.cs
+++ AssignmentManagementApp/Assignment.cs
@@ -6,15 +6,7 @@ using System.Threading.Tasks;
 
 namespace AssignmentManagementApp
 {
-    class Assignment
+    public class Assignment
     {
-		public string AssignmentTitle { get; set; }
-		public DateTime DueDate { get; set; }
-
-		public Assignment(string assignmentTitle, DateTime dueDate)
-		{
-			AssignmentTitle = assignmentTitle;
-			DueDate = dueDate;
-		}
-	}
+    }
 }
---
+++ AssignmentManagementApp_Tests/Assignment_tests.cs
@@ -0,0 +1,16 @@
+﻿using AssignmentManagementApp;
+using Xunit.Sdk;
+
+namespace AssignmentManagementApp_Tests
+{
+	public class Assignment_tests
+	{
+		[Fact]
+		public void IsAssignmentValid()
+		{
+			Assignment tsl = new Assignment();
+			Assert.IsType<Assignment>(tsl);
+			// would need to be of type Assignment to pass, but instead is passed type object
+		}
+	}
+}
